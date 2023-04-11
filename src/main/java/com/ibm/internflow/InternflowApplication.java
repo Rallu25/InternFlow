aaf63138d701c9c3fd4e245595e5417a34dc3d6c
Raluca <raluca.osman02@e-uvt.ro>
2023-04-11 15:27:17 +0300

    initial commit

---
+++ src/main/java/com/ibm/internflow/InternflowApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.internflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/internflow/InternflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.internflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
