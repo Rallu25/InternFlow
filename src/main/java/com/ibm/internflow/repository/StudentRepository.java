2a43b5e4f45d4dfd7df2746936ab26bb47fc166a
BiancaBarbaliu <bianca.barbaliu02@e-uvt.ro>
2023-05-18 03:49:09 +0300

    Add getStudentByTeam method in StudentService

--- src/main/java/com/ibm/internflow/repository/StudentRepository.java
+++ src/main/java/com/ibm/internflow/repository/StudentRepository.java
@@ -4,7 +4,10 @@ import com.ibm.internflow.entity.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 
 public interface StudentRepository extends JpaRepository<StudentEntity, Long>{
+    List<StudentEntity> findByTeam_TeamId(Long teamId);
 }
